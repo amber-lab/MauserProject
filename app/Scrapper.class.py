79c0f76d67702e6f127d4b82dc250b4b0b383b88
amber-lab <leoamber27@gmail.com>
2021-03-31 09:10:20 +0100

    Testing

---
+++ app/Scrapper.class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 18:10:19 2021
+
+@author: L.A.B
+"""
+
+
+class Scrapper:
+    def self.__init__(self, url, search_func, search_redirect):
+        self.url = url
+
+
+
